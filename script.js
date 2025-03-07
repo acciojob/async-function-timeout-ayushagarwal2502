ae35acde8141e93e1591a17f0c0ea3c073a1eeeb
ayushagarwal2502 <159728868+ayushagarwal2502@users.noreply.github.com>
2025-03-07 22:47:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+async function myDisplay() {
+	let Delay=document.getElementById("delay").value;
+	let text=document.getElementById("text").value;
+  let myPromise = new Promise(function(resolve) {
+    setTimeout(function() {resolve(text);}, Delay);
+  });
+  document.getElementById("output").innerHTML = await myPromise;
+}
